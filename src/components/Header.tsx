e4e15c68c7dc72c107ece0744782a52f9dc52c92
Orell Wandji <orell.wandji@haven-app.co>
2025-09-11 19:08:17 +0200

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import Button from "./Button";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-around my-5 mx-25">
+      <h1 className="text-4xl text-blue-950 font-bold">Immo Plus</h1>
+      <ul className="flex gap-10 font-medium">
+        <li>Accueil</li>
+        <li>Biens</li>
+        <li>Contact</li>
+      </ul>
+      <Button className="rounded-lg text-white font-medium py-2 px-4 bg-blue-950">
+        Se connecter
+      </Button>
+    </header>
+  );
+}
