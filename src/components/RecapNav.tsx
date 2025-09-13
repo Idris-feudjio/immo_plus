721af0ebed75dd930ed4b6711e09ad2ef8f60d39
Orell Wandji <orell.wandji@haven-app.co>
2025-09-13 22:31:49 +0200

    Add navbar for the future property page

---
+++ src/components/RecapNav.tsx
@@ -0,0 +1,17 @@
+export default function RecapNav() {
+  return (
+    <div className="bg-blue-950 text-white w-1/5 h-screen p-10 text-center space-y-10">
+      <h1 className="text-4xl font-bold">Immo Plus</h1>
+      <nav>
+        <ul className="text-xl space-y-4">
+          <li>Tableau de bord</li>
+          <li>Biens</li>
+          <li>Locations</li>
+          <li>Paiements</li>
+          <li>Documents</li>
+          <li>Messages</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
