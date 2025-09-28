d5cfe36cad92ab4eec3ada317b6df4c3874654a5
orell.wandji <orell.wandji@hes-so.ch>
2025-09-28 13:40:50 +0200

    Modify data

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,38 +1,22 @@
+import type { Property } from "../fake_data/fake_data";
 import Button from "./Button";
 
 type CardProps = {
-  imgsrc: string;
-  localType: string;
-  city: string;
-  street: string;
-  price: number;
-  disponibility: string;
-  onClick: () => void;
+  property: Property;
 };
 
-export default function Card({
-  imgsrc,
-  localType,
-  city,
-  street,
-  price,
-  disponibility,
-  onClick,
-}: CardProps) {
+export default function Card({ property }: CardProps) {
   return (
     <div className=" border border-gray-50 rounded-xl grid grid-rows-2 w-70 h-140 overflow-hidden">
-      <img src={imgsrc} className="w-full h-full object-cover" />
+      <img src={property.imgsrc} className="w-full h-full object-cover" />
       <div className="grid grid-rows-5 px-4 py-4 ">
-        <p className="text-2xl font-medium">{localType}</p>
+        <p className="text-2xl font-medium">{property.local}</p>
         <p className="text-xl font-light">
-          {city} ~ {street}
+          {property.city} ~ {property.street}
         </p>
-        <p className="text-2xl font-medium">{price} £/mois</p>
-        <p className="text-lg">{disponibility}</p>
-        <Button
-          onClick={onClick}
-          className="rounded-lg text-white font-medium py-2 px-4 bg-blue-950"
-        >
+        <p className="text-2xl font-medium">{property.price} £/mois</p>
+        <p className="text-lg">{property.disponibility}</p>
+        <Button className="cursor-pointer rounded-lg text-white font-medium py-2 px-4 bg-blue-950">
           Voir les détails
         </Button>
       </div>
